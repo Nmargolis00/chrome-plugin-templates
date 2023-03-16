e87f0cc16676fc5041e5e142fa93d3495de30326
Nate Margolis <Nate.Margolis@corp.boatechnology.com>
2023-03-16 12:07:04 -0600

    Got input to register and fixed previous issue of input not registering

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,6 @@
 const savebtn = document.querySelector('.save-button');
-const templateDisplay = document.querySelector('.template-dispay');
 let templates = [];
+
 
 
 //Load previous saved templates from local storage
@@ -11,28 +11,28 @@ let templates = [];
 
 function saveTemplate (){
     let templateEl = document.querySelector('#template').value;
+    console.log(templateEl);
     //Only save a unique value
     if(!templateEl.includes(templates)) {
         templates.push(templateEl);
         localStorage.setItem("templates", JSON.stringify(templates))
-        console.log(templates);
+        
     }
-    displayTemplate(templates);
+    console.log(templates);
 }
 
 //Template literal for displaying saved information. Get something from boot strap and add in a delete buttong and a copy to clipboard button
 
-function displayTemplate (templates) {
-    templateDisplay.innerHTML = "";
-    for (let index = 0; index < templates.length; index++) {
+// function displayTemplate (templates) {
+//     templateDisplay.innerHTML = "";
+//     for (let index = 0; index < templates.length; index++) {
         // You need to add in how you want the data displayed
-    }
-}
+
 
 //Loop through array to display information
 
 
 //Event Listeners
-saveBtn.addEventListener("click", function (){
+savebtn.addEventListener("click", function (){
     saveTemplate();
 });
