c992a0511f3acc2d25d148e36ab21418b1cfe175
Nate Margolis <Nate.Margolis@corp.boatechnology.com>
2023-04-12 10:01:12 -0600

    added in some more code for manifest file and js

--- popup/popup.js
+++ popup/popup.js
@@ -23,6 +23,31 @@ function saveTemplate (){
 
 //Template literal for displaying saved information. Get something from boot strap and add in a delete buttong and a copy to clipboard button
 
+function displayTemplate () {
+    for (let index = 0; index < templates.length; index++) {
+        const element = array[index];
+
+
+
+
+        let templateDisplay = $(`
+        <p class = templates id = "templateToCopy"> ${templates.list[0]} </p>
+        <button type="button" class="btn btn-outline-primary copy-button">
+      Copy to Clipboard
+    </button>
+        `)
+    }
+}
+
+function copyText () {
+    let copyTemplate = document.getElementById('templateToCopy');
+    copyTemplate.select();
+    copyTemplate.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyTemplate.value);
+    alert("Copied to Clipboard!")
+
+}
+
 // function displayTemplate (templates) {
 //     templateDisplay.innerHTML = "";
 //     for (let index = 0; index < templates.length; index++) {
