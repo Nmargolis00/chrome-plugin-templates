d2ba32c0bc4a72abd0322ccebf76e62799422653
Nate Margolis <Nate.Margolis@corp.boatechnology.com>
2023-04-12 13:55:55 -0600

    Got a little bit better psuedo code and better resources

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,26 @@
 const savebtn = document.querySelector('.save-button');
-let templates = [];
+let templateText = document.querySelector('#template').value;
+let templateTitle = document.querySelector('#template-title').value;
+let templates = [
+    {
+    title: `${templateTitle}`,
+    text: `${templateText}`
+}];
 
+//Could be best to save it to a data base. Pocketbase is a good spot to check it out
+
+//This would need a models folder to outline the data
+
+//Create an object that takes in a title and text
+
+//Add input values into that object with the correct key
+
+//Store that locally or however chrom extension wants you to store it
+
+//Loop through and display it as an accordian from bootstrap
+//*for of loop?
+
+//Display template literal and append
 
 
 //Load previous saved templates from local storage
@@ -16,16 +36,18 @@ function init() {
 //Save input to local storage and add to empty array
 
 function saveTemplate (){
-    let templateEl = document.querySelector('#template').value;
-    console.log(templateEl);
-    //Only save a unique value
-    if(!templateEl.includes(templates)) {
-        templates.push(templateEl);
-        localStorage.setItem("templates", JSON.stringify(templates))
-        
+console.log(templateText);
+
+    chrome.storage.local.set({ title: `${templateTitle}`, text: `${templateText}` }).then(() => {
+        console.log("Value is set to " + value);
+      });
+      
+      chrome.storage.local.get(["title", "text"]).then((result) => {
+        console.log("Value currently is " + result.title);
+      });
     }
-    console.log(templates);
-}
+//How do I save the title and the text together?
+
 
 //Template literal for displaying saved information. Get something from boot strap and add in a delete buttong and a copy to clipboard button
 //* Add a title with the copy to clipboard button next to that?
@@ -36,12 +58,12 @@ function displayTemplate () {
         <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        ${templateTitle}
+        ${templates.title[0]}
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        ${templates.list[0]}
+        ${templates.text[0]}
         <button type="button" class="btn btn-outline-primary copy-button">
       Copy to Clipboard
       </button>
@@ -49,7 +71,7 @@ function displayTemplate () {
     </div>
   </div>
         `)
-        $('.form-control').append(templateDisplay)
+        $('.savedContent').append(templateDisplay)
     }
 }
 
@@ -62,10 +84,13 @@ function copyText () {
     navigator.clipboard.writeText(copyTemplate.value);
     alert("Copied to Clipboard!")
 
-}
+};
 
 
 //Event Listeners
 savebtn.addEventListener("click", function (){
     saveTemplate();
+    displayTemplate();
 });
+
+//Will need event listener for the copy text function
